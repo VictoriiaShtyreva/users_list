6acc57c0b3676278f63d29fb2fa3fd68e8925613
Viktoriia <vika1411akva0903@icloud.com>
2022-11-28 16:54:03 +0200

    feat: add Button component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+const Button = (props) => {
+  const handleClick = () => {};
+  return (
+    <div>
+      <button type="submit" onClick={handleClick}></button>
+    </div>
+  );
+};
+
+export default Button;
